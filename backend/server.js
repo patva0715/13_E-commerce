2e1c78b02264dcc816cb1b64091381af0f3d4026
saish0 <patrickvalera50@yahoo.com>
2022-01-09 11:45:21 -0800

    test

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ app.use('/api/products',productRoutes)
 // SERVE BUILD FILES TO CLIENT
 if(process.env.NODE_ENV==='production'){
     app.use(express.static(path.join(__dirname,'/frontend/build')))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-    })
+    // app.get('*',(req,res)=>{
+    //     res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    // })
 }else{
     app.get('/api',(req,res)=>{
     res.send("api")
