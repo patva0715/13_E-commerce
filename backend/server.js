62a11e3f4438c594ed4c0e6be9715b9d7043e21f
saish0 <patrickvalera50@yahoo.com>
2022-01-09 11:57:13 -0800

    fsfj

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,13 @@ dotenv.config()
 connectDB()
 const app = express()
 console.log(__dirname)
-app.use(express.static(path.join(__dirname,'/frontend/build')))
+app.use(express.static(path.join(__dirname,'/../frontend/build')))
   app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
     })
 app.use(express.json())
 app.use('/api/products',productRoutes)
-// SERVE BUILD FILES TO CLIENT
+
 
 
 if(process.env.NODE_ENV==='production'){
