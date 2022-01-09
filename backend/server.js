038c093e8c92b1549dc52272b7bd7fa8035f2195
saish0 <patrickvalera50@yahoo.com>
2022-01-09 14:33:14 -0800

    OK ITS GOOD FROM THIS POINT!

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,16 @@ app.use(express.json())
 // ROUTES HERE
 app.use('/api/products', productRoutes)
 
-        if (process.env.NODE_ENV === 'production') {
-            app.use(express.static(path.join(__dirname, '/../frontend/build')))
-            app.get('*', (req, res) => {
-                res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-            })
-        } else {
-            app.get('/api', (req, res) => {
-                res.send("api")
-            })
-        }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/../frontend/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../','frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('/api', (req, res) => {
+        res.send("api")
+    })
+}
 
-            app.listen(process.env.PORT || 5000, console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`))
+app.listen(process.env.PORT || 5000, console.log(`SERVER IS RUNNING ON PORT ${process.env.PORT}`))
 
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,10 +1,10 @@
-import {useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Box, Divider, FormControl, OutlinedInput, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import '../myStyles/search.css'
 
-function Search({handleClose}) {
+function Search({ handleClose }) {
   let navigate = useNavigate()
   const [suggestions, setSuggestions] = useState([])
   const [open, setOpen] = useState(false)
@@ -35,30 +35,31 @@ function Search({handleClose}) {
 
   return (
     <>
-    
-          <Box sx={{ margin: '0 auto', px: 2,py:3, bgcolor: 'white', borderRadius: '1rem', maxWidth: '40ch',width:'100%' }} onClick={(e) => e.stopPropagation()}>
-            <FormControl sx={{ width:'100%' }}>
-              <OutlinedInput placeholder="Enter Product Name..." onChange={handleChange} />
-              {/* <MyFormHelperText /> */}
-            </FormControl>
-            <Box sx={{ minHeight: '400px', pt:2 }}>
-              {suggestions && suggestions.map((item) => {
-                let itemName=item.name.toLowerCase().replace(/ /g, '')
-                return (
-                <>
-                  <Box display='flex' onClick={() => handleItemClick(item._id)} sx={{p:'-15px', alignItems: 'center' }}>
-                    <Box sx={{ width: '80px', aspectRatio: '1/1' }}>
-                      <img className='image-fit-contain' src={`/images/products/${itemName}/${itemName}.jfif`} alt='search-item' />
-                    </Box>
-                    <Typography variant='body1' sx={{ ml: 2 }}>
-                      {item.name}
-                    </Typography>
+
+      <Box sx={{ margin: '0 auto', px: 2, py: 3, bgcolor: 'white', borderRadius: '1rem', maxWidth: '40ch', width: '100%' }} onClick={(e) => e.stopPropagation()}>
+        <FormControl sx={{ width: '100%' }}>
+          <OutlinedInput placeholder="Enter Product Name..." onChange={handleChange} />
+          {/* <MyFormHelperText /> */}
+        </FormControl>
+        <Box sx={{ minHeight: '400px', pt: 2 }}>
+          {suggestions && suggestions.map((item) => {
+            let itemName = item.name.toLowerCase().replace(/ /g, '')
+            return (
+              <>
+                <Box display='flex' onClick={() => handleItemClick(item._id)} sx={{ p: '-15px', alignItems: 'center', cursor: 'pointer' }}>
+                  <Box sx={{ width: '80px', aspectRatio: '1/1' }}>
+                    <img className='image-fit-contain' src={`/images/products/${itemName}/${itemName}.jfif`} alt='search-item' />
                   </Box>
-                  <Divider />
-                </>
-              )})}
-            </Box>
-          </Box>
+                  <Typography variant='body1' sx={{ ml: 2 }}>
+                    {item.name}
+                  </Typography>
+                </Box>
+                <Divider />
+              </>
+            )
+          })}
+        </Box>
+      </Box>
     </>
 
   );
