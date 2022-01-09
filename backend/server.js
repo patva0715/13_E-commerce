08d0f33cd4b2c143bafab32d0d4b680afb954b29
saish0 <patrickvalera50@yahoo.com>
2022-01-09 02:41:58 -0800

    server fix

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,16 @@ const path = require('path')
 const colors = require('colors')
 const connectDB = require('./dbconfig.js')
 const productRoutes = require('./routes/productRoutes.js')
+
 dotenv.config()
 connectDB()
 const app = express()
 
 app.use(express.json())
 app.use('/api/products',productRoutes)
-const __dirname=path.resolve()
 
 if(process.env.NODE_ENV==='production'){
+    // const __dirname=path.resolve()
     app.use(express.static(path.join(__dirname,'/frontend/build')))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
